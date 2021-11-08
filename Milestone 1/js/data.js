70f981a2ffbd962405a2d07989fd0fbe081bd2a1
davidecarta470 <cartadavi@gmail.com>
2021-11-08 18:16:11 +0100

    correction color

--- Milestone 1/js/data.js
+++ Milestone 1/js/data.js
@@ -116,6 +116,26 @@ const arrayColorType =['yellow','blue','purple']
 const row = document.querySelector('.row');
 console.log(row)
 
+
+let select = document.getElementById('select').value;
+	const iconsAnimals = icons.filter((icon)=>{
+		return icon.type==='animal'
+	})
+	console.log(iconsAnimals)
+
+
+	const iconsVegetables = icons.filter((icon)=>{
+		return icon.type==='vegetable'
+	})
+	console.log(iconsVegetables.color)
+
+
+	const iconsUsers = icons.filter((icon)=>{
+		return icon.type==='user'
+	})
+	console.log(iconsUsers)
+
+
 for(let i=0; i<icons.length ; i++){
 	let square = document.createElement('div');
 	square.className = 'square';
@@ -125,17 +145,22 @@ for(let i=0; i<icons.length ; i++){
 	row.append(square);
 }
 
+	
 
+
+
+
+console.log(iconsVegetables.color)
+// FUNZIONE COLORI ELEMENTI 
 function itemsColor(iconsType,squareStyle){
-
 	if(iconsType ==='vegetable'){
-		squareStyle.style = `color:${arrayColorType[0]}`;
+		squareStyle.style = `color:${iconsVegetables[0].color}`;
 	 }
  	if(iconsType ==='animal'){
-		squareStyle.style = `color:${arrayColorType[1]}`;
+		squareStyle.style = `color:${iconsAnimals[0].color}`;
 	 }
 	 if(iconsType ==='user'){
-		squareStyle.style = `color:${arrayColorType[2]}`;
+		squareStyle.style = `color:${iconsUsers[0].color}`;
 	 }
 }
 
