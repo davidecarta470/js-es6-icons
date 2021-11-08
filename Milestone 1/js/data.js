7044a9b57b1a92ec678bc86e69ae179e4dcee394
davidecarta470 <cartadavi@gmail.com>
2021-11-08 17:17:17 +0100

    function color items

--- Milestone 1/js/data.js
+++ Milestone 1/js/data.js
@@ -121,7 +121,7 @@ for(let i=0; i<icons.length ; i++){
 	square.className = 'square';
 	square.classList.add('col-2')
 	square.innerHTML=`<i class="fas fa-${icons[i].name}"></i>`
-	itemsColor(icons[i].type,square.style)
+	itemsColor(icons[i].type,square)
 	row.append(square);
 }
 
@@ -129,13 +129,13 @@ for(let i=0; i<icons.length ; i++){
 function itemsColor(iconsType,squareStyle){
 
 	if(iconsType ==='vegetable'){
-		squareStyle = `color:${arrayColorType[0]}`;
+		squareStyle.style = `color:${arrayColorType[0]}`;
 	 }
  	if(iconsType ==='animal'){
-		squareStyle = `color:${arrayColorType[1]}`;
+		squareStyle.style = `color:${arrayColorType[1]}`;
 	 }
-	 if(iconsType ==='vegetable'){
-		squareStyle = `color:${arrayColorType[2]}`;
+	 if(iconsType ==='user'){
+		squareStyle.style = `color:${arrayColorType[2]}`;
 	 }
 }
 
