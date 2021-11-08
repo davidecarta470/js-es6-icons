ea2a8de157546a06644c75eac63d57cb6e81589c
davidecarta470 <cartadavi@gmail.com>
2021-11-08 17:14:24 +0100

    html structure

---
+++ Milestone 1/js/data.js
@@ -0,0 +1,141 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+const arrayColorType =['yellow','blue','purple']
+const row = document.querySelector('.row');
+console.log(row)
+
+for(let i=0; i<icons.length ; i++){
+	let square = document.createElement('div');
+	square.className = 'square';
+	square.classList.add('col-2')
+	square.innerHTML=`<i class="fas fa-${icons[i].name}"></i>`
+	itemsColor(icons[i].type,square.style)
+	row.append(square);
+}
+
+
+function itemsColor(iconsType,squareStyle){
+
+	if(iconsType ==='vegetable'){
+		squareStyle = `color:${arrayColorType[0]}`;
+	 }
+ 	if(iconsType ==='animal'){
+		squareStyle = `color:${arrayColorType[1]}`;
+	 }
+	 if(iconsType ==='vegetable'){
+		squareStyle = `color:${arrayColorType[2]}`;
+	 }
+}
+
